4cfe89aab6786ab1bbc6a8d318fab044970684ae
Julien Perrenoud <julien.perrenoud@epfl.ch>
2015-04-08 14:01:42 +0200

    Modify project CMake properties, should be working w/ working directory set to /source

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -29,7 +29,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/jfperren/Git/opengl-terrain/build/terrain_generating/cmake_install.cmake")
+  include("C:/Users/jfperren/Git/opengl-terrain/build/source/cmake_install.cmake")
 
 endif()
 
---
+++ source/main.cpp
@@ -0,0 +1,188 @@
+#include "icg_common.h"
+#include "trackball/trackball.h"
+#include "grid/grid.h"
+
+using namespace std;
+
+Grid grid;
+
+int WIDTH = 800;
+int HEIGHT = 600;
+
+mat4 projection_matrix;
+mat4 view_matrix;
+mat4 trackball_matrix;
+
+Trackball trackball;
+
+// Constants
+const float kZoomFactor = 2;
+
+// Used to store old values in computation
+mat4 old_trackball_matrix;
+mat4 old_view_matrix;
+float zoom_start_y;
+
+mat4 OrthographicProjection(float left, float right, float bottom, float top, float near, float far){
+    assert(right > left);
+    assert(far > near);
+    assert(top > bottom);
+    mat4 ortho = mat4::Zero();
+    ortho(0, 0) = 2.0f / (right - left);
+    ortho(1, 1) = 2.0f / (top - bottom);
+    ortho(2, 2) = -2.0f / (far - near);
+    ortho(3, 3) = 1.0f;
+    ortho(1, 3) = -(right + left) / (right - left);
+    ortho(2, 3) = -(top + bottom) / (top - bottom);
+    ortho(2, 3) = -(far + near) / (far - near);
+    return ortho;
+}
+
+mat4 PerspectiveProjection(float fovy, float aspect, float near, float far){
+    mat4 projection = mat4::Identity();
+
+	float top = near * tan(1.0f / 2.0f * fovy);
+	float right = aspect * top;
+
+	projection <<
+		near / right, 0.0f, 0.0f, 0.0f,
+		0.0f, near / top, 0.0f, 0.0f,
+		0.0f, 0.0f, (far + near) / (near - far), 2 * far*near / (near - far),
+		0.0f, 0.0f, -1.0f, 0.0f;
+
+    return projection;
+}
+
+mat4 LookAt(vec3 eye, vec3 center, vec3 up) {
+    // We need a function that converts from world coordinates into camera coordiantes.
+    //
+    // Cam coords to world coords is given by:
+    // X_world = R * X_cam + eye
+    //
+    // inverting it leads to:
+    //
+    // X_cam = R^T * X_world - R^T * eye
+    //
+    // Or as a homogeneous matrix:
+    // [ r_00 r_10 r_20 -r_0*eye
+    //   r_01 r_11 r_21 -r_1*eye
+    //   r_02 r_12 r_22 -r_2*eye
+    //      0    0    0        1 ]
+
+    vec3 z_cam = (eye - center).normalized();
+    vec3 x_cam = up.cross(z_cam).normalized();
+    vec3 y_cam = z_cam.cross(x_cam);
+
+    mat3 R;
+    R.col(0) = x_cam;
+    R.col(1) = y_cam;
+    R.col(2) = z_cam;
+
+    mat4 look_at = mat4::Zero();
+    look_at.block(0, 0, 3, 3) = R.transpose();
+    look_at(3, 3) = 1.0f;
+    look_at.block(0, 3, 3, 1) = -R.transpose() * (eye);
+    return look_at;
+}
+
+// Gets called when the windows is resized.
+void resize_callback(int width, int height) {
+    WIDTH = width;
+    HEIGHT = height;
+
+    std::cout << "Window has been resized to " << WIDTH << "x" << HEIGHT << "." << std::endl;
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    GLfloat top = 1.0f;
+    GLfloat right = (GLfloat)WIDTH / HEIGHT * top;
+    //projection_matrix = OrthographicProjection(-right, right, -top, top, -10.0, 10.0f);
+    projection_matrix = PerspectiveProjection(45.0f, (GLfloat)WIDTH / HEIGHT, 0.1f, 100.0f);
+}
+
+void init(){
+    // Sets background color.
+    glClearColor(/*gray*/ .937,.937,.937, /*solid*/1.0);
+    
+    grid.init();
+
+    // Enable depth test.
+    glEnable(GL_DEPTH_TEST);
+    
+    // view_matrix = LookAt(vec3(2.0f, 2.0f, 4.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+    view_matrix = Eigen::Affine3f(Eigen::Translation3f(0.0f, 0.0f, -4.0f)).matrix();
+
+    trackball_matrix = mat4::Identity();
+    check_error_gl();
+}
+
+// Gets called for every frame.
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Draw a quad on the ground.
+    mat4 quad_model_matrix = mat4::Identity();
+    grid.draw(trackball_matrix * quad_model_matrix, view_matrix, projection_matrix);
+
+    check_error_gl();
+}
+
+// Transforms glfw screen coordinates into normalized OpenGL coordinates.
+vec2 transform_screen_coords(int x, int y) {
+    return vec2(2.0f * (float)x / WIDTH - 1.0f,
+                1.0f - 2.0f * (float)y / HEIGHT);
+}
+
+void mouse_button(int button, int action) {
+	// Rotation
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        int x_i, y_i;
+        glfwGetMousePos(&x_i, &y_i);
+        vec2 p = transform_screen_coords(x_i, y_i);
+        trackball.begin_drag(p.x(), p.y());
+		// Store the current state of the model matrix.
+        old_trackball_matrix = trackball_matrix;  
+    }
+
+	// Zoom
+	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
+		// Get 2-d vector of the mouse position on screen in range [-1, 1]
+		int x_i, y_i;
+		glfwGetMousePos(&x_i, &y_i);
+		vec2 p = transform_screen_coords(x_i, y_i);
+
+		// Store y value & current state of the view matrix
+		zoom_start_y = p[1];
+		old_view_matrix = view_matrix;
+	}
+}
+
+void mouse_pos(int x, int y) {
+    // Rotation
+    if (glfwGetMouseButton(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+        vec2 p = transform_screen_coords(x, y);
+		// Compute the new trackball_matrix
+		trackball_matrix = trackball.drag(p[0], p[1]) * old_trackball_matrix;
+    }
+
+    // Zoom
+    if (glfwGetMouseButton(GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+		// Get position on screen
+		vec2 p = transform_screen_coords(x, y);
+		// Zoom proportional to the distance
+		float zoom = p[1] - zoom_start_y;
+		// Apply it to translate the view matrix along the z-axis
+		view_matrix = old_view_matrix * Eigen::Affine3f(Eigen::Translation3f(vec3(0.0f, 0.0f, zoom))).matrix();
+    }
+}
+
+int main(int, char**){
+    glfwInitWindowSize(WIDTH, HEIGHT);
+    glfwCreateWindow("Trackball");
+    glfwDisplayFunc(display);
+    glfwSetWindowSizeCallback(&resize_callback);
+    glfwSetMouseButtonCallback(mouse_button);
+    glfwSetMousePosCallback(mouse_pos);
+    init();
+    glfwMainLoop();
+    return EXIT_SUCCESS;
+}
