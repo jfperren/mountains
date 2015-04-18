ee0c0842fb3fc8288d9ada1395ba7a60dab36d8b
Julien Perrenoud <julien.perrenoud@epfl.ch>
2015-04-18 19:26:47 +0200

    Clear code in Water.h

--- source/water/Water.h
+++ source/water/Water.h
@@ -1,10 +1,7 @@
 #pragma once
 #include "icg_common.h"
 
-
 class Water {
-private:
-	static const int grid_dim_ = 2048;
 
 protected:
 	GLuint _vao;          ///< vertex array object
@@ -12,15 +9,11 @@ protected:
 	GLuint _vbo_index;    ///< memory buffer for indice
 	GLuint _vbo;
 	GLuint _pid;          ///< GLSL shader program ID
-	GLuint _height_map;    ///< HeightMap Texture ID
-	GLuint _color;        ///< Color Texture ID
 	GLuint _num_indices;  ///< number of vertices to render
 
 public:
-
-	int get_vertex_index(int i, int j, int grid_dim) {
-		return i + j * grid_dim;
-	}
+	float height = 0.5;
+	float alpha = 0.5;
 
 	void init(){
 		// Compile the shaders
@@ -36,7 +29,6 @@ public:
 		{
 			std::vector<GLfloat> vertices;
 			std::vector<GLuint> indices;
-
 
 			vertices.push_back(-1);
 			vertices.push_back(-1);
@@ -87,50 +79,14 @@ public:
 		glDeleteProgram(_pid);
 	}
 
-	void setHeightMap(GLuint* height_map) {
-		///--- Texture coordinates
-		const GLfloat vtexcoord[] = { /*V1*/ 0.0f, 0.0f,
-			/*V2*/ 1.0f, 0.0f,
-			/*V3*/ 0.0f, 1.0f,
-			/*V4*/ 1.0f, 1.0f };
-
-		///--- Buffer
-		glGenBuffers(1, &_vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, _vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vtexcoord), vtexcoord, GL_STATIC_DRAW);
-
-		///--- Attribute
-		GLuint vtexcoord_id = glGetAttribLocation(_pid, "vtexcoord");
-		glEnableVertexAttribArray(vtexcoord_id);
-		glVertexAttribPointer(vtexcoord_id, 2, GL_FLOAT, DONT_NORMALIZE, ZERO_STRIDE, ZERO_BUFFER_OFFSET);
-
-		// Pass texture to instance
-		this->_height_map = *height_map;
-
-		GLuint tex_id = glGetUniformLocation(_pid, "tex");
-		glUniform1i(tex_id, 0 /*GL_TEXTURE0*/);
-	}
-
-	void setColor(GLuint* color){
-		// Pass texture to instance
-		this->_color = *color;
-		GLuint tex_id = glGetUniformLocation(_pid, "color1D");
-		glUniform1i(tex_id, 1 /*GL_TEXTURE1*/);
-	}
 
 	void draw(const mat4& model, const mat4& view, const mat4& projection){
 		glUseProgram(_pid);
 		glBindVertexArray(_vao);
 
-		// Texture uniforms
-		glUniform1i(glGetUniformLocation(_pid, "heightmap"), 0 /*GL_TEXTURE0*/);
-		glUniform1i(glGetUniformLocation(_pid, "color1D"), 1 /*GL_TEXTURE1*/);
-
-		// Bind textures
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, _height_map);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, _color);
+		// Send Uniforms
+		glUniform1f(glGetUniformLocation(_pid, "height"), height);
+		glUniform1f(glGetUniformLocation(_pid, "alpha"), alpha);
 
 		// Setup MVP
 		mat4 MVP = projection*view*model;
