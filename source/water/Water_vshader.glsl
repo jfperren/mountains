e9ee51cbbd81fbe8e372bf70ad8f6a814d73bdbf
Julien Perrenoud <julien.perrenoud@epfl.ch>
2015-04-22 20:53:20 +0200

    Before implementing water on borders

--- source/water/Water_vshader.glsl
+++ source/water/Water_vshader.glsl
@@ -6,11 +6,11 @@ in vec2 vertex_tex_coord;
 out vec2 uv;
 
 uniform mat4 mvp;
-uniform float height;
+uniform float water_height;
 
 void main() {
-    vec3 pos_3d = vec3(vertex_pos.x, height, -vertex_pos.y);
-
+    //vec3 pos_3d = vec3(vertex_pos.x, water_height, -vertex_pos.y);
+	vec3 pos_3d = vertex_pos;
     gl_Position = mvp * vec4(pos_3d, 1.0);
 
 	uv = vertex_tex_coord;
