875606113b4d033f136aa4a5d9972fcc104197fa
Robin Genolet <robin.genolet@epfl.ch>
2015-05-24 00:30:04 +0200

    Can take screenshots with F12

--- src/tw/io.h
+++ src/tw/io.h
@@ -8,9 +8,12 @@
 #include <iterator>
 #include <sstream>
 #include <algorithm>
+#include <ctime>
 
 #include "icg_common.h"
 #include "../app/constants.h"
 
 void writeFile(string file_name, NoiseParams *noise_params, WaterParams *water_params);
 void loadFromFile(string file_name, NoiseParams *noise_params, WaterParams *water_params);
+bool save_screenshot(string file_name, int width, int height);
+string get_unique_name();
